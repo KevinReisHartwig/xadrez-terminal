fb602ec4f369d1e0e2aca75f1fb1a60a6de30402
Kevin dos Reis Hartwig <kevindosreishartwig1221@gmail.com>
2023-12-05 19:48:48 -0300

    Começando a implementar um metodo que imprime o tabuleiro

--- xadrez-console/Tabuleiro/Tabuleiro.cs
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -9,5 +9,9 @@
             this.colunas = colunas;
             pecas = new Peca[linhas, colunas];
         }
+
+        public Peca peca(int linhas, int colunas) {
+            return pecas[linhas, colunas];
+        }
     }
 }
